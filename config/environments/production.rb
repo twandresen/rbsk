e638acc7206dd04c88d39b719805c1991b8fc66e
Tom Andresen <twandresen@gmail.com>
2019-03-25 21:05:13 +0000

    add unicorn env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,11 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+	config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
-  #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -35,6 +36,9 @@ Rails.application.configure do
   config.assets.digest = true
 
   config.less.compress = true
+
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_files = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
