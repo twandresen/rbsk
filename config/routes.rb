7219f82bc99463910481baa91f376ae50199eba5
Lisa Ramirez <lmframirez@gmail.com>
2019-03-25 14:35:00 +0000

    additional pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
 	get "traditional" => "home#traditional"
 	get "modern" => "home#modern"
+        get "modern_red" => "home#modern_red"
 	get "visual" => "home#visual"
+	get "services" => "home#services"
+	get "about" => "home#about"
 	get "css" => "home#css"
 
 end
