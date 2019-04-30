edbd209f7247f832ef9bf787f8c6037614bd5947
Lisa Ramirez <lmframirez@gmail.com>
2019-04-30 02:04:11 +0000

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
         get "new-adults" => "home#new-adults"
         get "seniors" => "home#seniors"
         get "service-members" => "home#service-members"
+        get "estate-planning" => "home#estate-planning"
         get "mental-health" => "home#mental-health"
 	get "css" => "home#css"
 
